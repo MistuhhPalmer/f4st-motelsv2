62c75693b282b0632fff54d51c0a05d47e6415b6
MistuhhPalmer <82214475+MistuhhPalmer@users.noreply.github.com>
2023-12-13 06:52:46 -0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -7,29 +7,29 @@ FastMotels.BlipColour = 83
 
 FastMotels.MotelCoords = vector3(-1857.82, -347.12, 49.84)
 
--- ODA ÜCRETİ AYARLARI -- 
-FastMotels.BasicRoomPrice = 1000 -- BASİT
-FastMotels.Room1Price = 2000  -- ORTA 
-FastMotels.Room2Price = 3000 -- BÜYÜK
--- ODA ÜCRETİ AYARLARI -- 
+-- ROOM FEE SETTINGS -- 
+FastMotels.BasicRoomPrice = 1000 -- BASIC
+FastMotels.Room1Price = 2000  -- MEDIUM 
+FastMotels.Room2Price = 3000 -- LARGE
+-- ROOM FEE SETTINGS -- 
 
--- BASİT ODA AYARLARI -- 
+-- BASIC ROOM SETTINGS -- 
 FastMotels.BasicRoomCoords = vector3(291.21, -925.03, -22.99)
 FastMotels.BasicRoomStash = vector3(283.39, -924.94, -23.20)
 FastMotels.BasicRoomOutfit = vector3(286.63, -922.49, -23.0)
 FastMotels.BasicRoomDoor = vector3(291.07, -924.82, -22.99)
--- BASİT ODA AYARLARI -- 
+-- BASIC ROOM SETTINGS -- 
 
--- ROOM 1 ODA AYARLARI -- 
+-- ROOM 1 SETTINGS -- 
 FastMotels.Room1Coords = vector3(266.17, -1007.03, -100.92)
 FastMotels.Room1DoorCoords = vector3(266.14, -1007.51, -101.01)
 FastMotels.Room1Stash = vector3(265.89, -999.36, -99.01)
 FastMotels.Room1Outfit = vector3(259.75, -1004.0, -99.01)
--- ROOM 1 ODA AYARLARI -- 
+-- ROOM 1 SETTINGS -- 
 
--- ROOM 2 ODA AYARLARI -- 
+-- ROOM 2 SETTINGS -- 
 FastMotels.Room2Coords = vector3(346.89, -1011.91, -99.2)
 FastMotels.Room2DoorCoords = vector3(346.51, -1013.23, -99.2)
 FastMotels.Room2Stash = vector3(351.94, -998.78, -99.2)
 FastMotels.Room2Outfit = vector3(350.7, -993.56, -99.2)
--- ROOM 2 ODA AYARLARI -- 
+-- ROOM 2 SETTINGS --
